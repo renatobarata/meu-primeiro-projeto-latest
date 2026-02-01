1cdcd3f9101abab9cd20bf80c8d78f9f253d4e12
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-01 20:22:24 -0300

    29. Template Variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TesteComponent } from './components/teste/teste.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TesteComponent, TemplateBindingComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TesteComponent, TemplateBindingComponent, TemplateVariablesComponent],
   template: `
     <!-- <router-outlet></router-outlet> -->
     <h1>Curso de Angular</h1>
     <app-new-component />
     <app-teste />
     <app-template-binding />
+    <app-template-variables />
   `,
 })
 export class AppComponent {
