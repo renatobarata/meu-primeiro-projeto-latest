7ab3c9cdeebe55ae78a7558f3b14c21e9f98ec6b
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-01 16:39:00 -0300

    23. Property Binding

--- src/app/components/template/template-binding/template-binding.component.ts
+++ src/app/components/template/template-binding/template-binding.component.ts
@@ -12,10 +12,18 @@ export class TemplateBindingComponent {
   public name = 'Dener Troquatte';
   public age = 32;
   public condition = this.age < 1 ? "Teste" : "Teste2";
+  public isDisabled = true;
+  public srcValue = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+  constructor() {
+    setTimeout(() => {
+      this.name = 'Renato Barata';
+      this.age = 28;
+      this.isDisabled = false;
+    }, 2000);
+  }
 
   public sum(val1: number, val2: number): number {
     return val1 + val2;
   }
-
-
 }
