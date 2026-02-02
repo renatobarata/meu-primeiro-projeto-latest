4412aeb7838918fe3b4ec71076d89488acef9935
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-01 22:32:10 -0300

    32. @for

--- src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,14 +1,25 @@
-import { NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [NgIf],
+  imports: [AsyncPipe, NgIf, NgFor],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
 export class TemplateControlFlowComponent {
 
   public isTrue = false;
+
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3'
+  ]).pipe(delay(3000));
+
+  public trackByFn(index: number) {
+    return index;
+  }
 }
