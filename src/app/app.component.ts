f198439eaaf1f40db961b502612b75b10735b497
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-01 22:04:37 -0300

    31. @if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TesteComponent } from './components/teste/teste.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TesteComponent, TemplateBindingComponent, TemplateVariablesComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NewComponent,
+    TesteComponent,
+    TemplateBindingComponent,
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent
+  ],
   template: `
     <!-- <router-outlet></router-outlet> -->
     <h1>Curso de Angular</h1>
@@ -17,6 +26,7 @@ import { TesteComponent } from './components/teste/teste.component';
     <app-teste />
     <app-template-binding />
     <app-template-variables />
+    <app-template-control-flow />
   `,
 })
 export class AppComponent {
