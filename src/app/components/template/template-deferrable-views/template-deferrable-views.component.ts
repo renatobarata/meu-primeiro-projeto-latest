d3cdc2efb6d8c39c3dafe751ce8e2696f09c2fbe
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-03 21:53:41 -0300

    39. @defer - when

--- src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
+++ src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -1,13 +1,22 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
 import { NewComponent } from '../../new-component/new-component.component';
 
 @Component({
   selector: 'app-template-deferrable-views',
   standalone: true,
-  imports: [NewComponent],
+  imports: [NewComponent, AsyncPipe],
   templateUrl: './template-deferrable-views.component.html',
   styleUrl: './template-deferrable-views.component.scss'
 })
 export class TemplateDeferrableViewsComponent {
 
+  public isTrue = false;
+
+  public loadingData$: Observable<string[]> = of([
+      'item 1',
+      'item 2',
+      'item 3'
+    ]).pipe(delay(3000));
 }
