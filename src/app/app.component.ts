37d3c3fbcd374e758cd195a1c28cc67c2d409fc2
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-03 21:17:03 -0300

    37. Introdução

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TesteComponent } from './components/teste/teste.component';
 
@@ -17,16 +18,19 @@ import { TesteComponent } from './components/teste/teste.component';
     TesteComponent,
     TemplateBindingComponent,
     TemplateVariablesComponent,
-    TemplateControlFlowComponent
+    TemplateControlFlowComponent,
+    TemplateDeferrableViewsComponent
   ],
   template: `
     <!-- <router-outlet></router-outlet> -->
+    <!-- <app-teste /> -->
+    <!-- <app-template-binding /> -->
+    <!-- <app-template-variables /> -->
+    <!-- <app-template-control-flow /> -->
+
     <h1>Curso de Angular</h1>
-    <app-new-component />
-    <app-teste />
-    <app-template-binding />
-    <app-template-variables />
-    <app-template-control-flow />
+    <app-template-deferrable-views>
+
   `,
 })
 export class AppComponent {
