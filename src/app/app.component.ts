c4ca032a5b1913e531935881722fdc14b204b203
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-05 08:57:27 -0300

    45. Usando o Signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
+import { SignalsComponent } from "./components/signals/signals.component";
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
@@ -19,17 +20,19 @@ import { TesteComponent } from './components/teste/teste.component';
     TemplateBindingComponent,
     TemplateVariablesComponent,
     TemplateControlFlowComponent,
-    TemplateDeferrableViewsComponent
-  ],
+    TemplateDeferrableViewsComponent,
+    SignalsComponent
+],
   template: `
     <!-- <router-outlet></router-outlet> -->
     <!-- <app-teste /> -->
     <!-- <app-template-binding /> -->
     <!-- <app-template-variables /> -->
     <!-- <app-template-control-flow /> -->
+    <!-- <app-template-deferrable-views> -->
 
     <h1>Curso de Angular</h1>
-    <app-template-deferrable-views>
+    <app-signals></app-signals>
 
   `,
 })
---
+++ src/app/components/signals/signals.component.ts
@@ -0,0 +1,31 @@
+import { Component, computed, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  standalone: true,
+  imports: [],
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.scss'
+})
+export class SignalsComponent {
+
+  public firstName = signal('Dener');
+  public lastName = signal('Troquatte');
+  public fullName = computed(() => {
+    return this.firstName() + ' ' + this.lastName();
+  });
+  public array = signal([1]);
+
+  constructor() { }
+
+  public updateName() {
+    return this.firstName.set('João');
+  }
+
+  public updateArray() {
+    this.array.update((oldValue: Array<number>) => {
+      console.log(oldValue);
+      return [...oldValue, oldValue.length + 1];
+    });
+  }
+}
