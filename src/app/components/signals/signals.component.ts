eed774265908221a3a4b3dbd0df38b8b81015d07
Renato Barata Gomes <renatobaratagomes@gmail.com>
2026-02-07 11:40:43 -0300

    46. Effect

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -16,7 +16,29 @@ export class SignalsComponent {
   });
   public array = signal([1]);
 
-  constructor() { }
+  /*
+    effect - raramente são necessários na maioria dos códigos,
+    mas podem ser úteis em circunstâncias específicas.
+    - Registro de dados sendo exibidos e quando eles mudam, seja para análise
+      ou como ferramenta de depuração.
+
+    - Manter os dados sincronizados com o window.localStorage.
+
+    - Adicionando comportamento DOM personalizado que não pode ser expresso com
+      sintaxe de modelo.
+
+    - Executar renderização personalizada em um <canvas>, biblioteca de gráficos
+      ou outra biblioteca de UI de terceiros.
+  */
+
+  constructor() {
+    effect(() => {
+      console.log(this.fullName());
+      if (this.firstName() === 'João') {
+        alert('O nome foi atualizado para João!');
+      }
+    });
+   }
 
   public updateName() {
     return this.firstName.set('João');
